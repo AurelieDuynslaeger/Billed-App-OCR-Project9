39890ad155f83dcc19339dee2a853f3b358e21c3
AureLeeLi <aduynslaeger@gmail.com>
2024-07-15 13:54:31 +0200

    🧪 test NewBill => first test : form diplay pass ✅

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -3,16 +3,48 @@
  */
 
 import { screen } from "@testing-library/dom"
+import userEvent from "@testing-library/user-event"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import { ROUTES_PATH } from "../constants/routes.js"
+import router from "../app/Router.js";
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    //avant chaque test, initialisation
+    beforeEach(() => {
+      //configuration du localStorage pour simuler l'utilisateur connecté
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }));
+
+      //création d'un élément root pour simuler le rendu de l'interface
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+
+      // Exécution du routeur pour simuler la navigation sur la page NewBill
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
+    })
+
+    test("Then the new bill form is displayed", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
+
+      expect(screen.getByTestId("form-new-bill")).toBeTruthy()
+      expect(screen.getByTestId("expense-type")).toBeTruthy()
+      expect(screen.getByTestId("expense-name")).toBeTruthy()
+      expect(screen.getByTestId("datepicker")).toBeTruthy()
+      expect(screen.getByTestId("amount")).toBeTruthy()
+      expect(screen.getByTestId("vat")).toBeTruthy()
+      expect(screen.getByTestId("pct")).toBeTruthy()
+      expect(screen.getByTestId("commentary")).toBeTruthy()
+      expect(screen.getByTestId("file")).toBeTruthy()
     })
   })
 })
+
