853e806e3059d307b86e00ea941e89ae9eabab08
AureLeeLi <aduynslaeger@gmail.com>
2024-08-01 10:46:03 +0300

    🚧 fix date

--- Billed-app-FR-Front/src/containers/Bills.js
+++ Billed-app-FR-Front/src/containers/Bills.js
@@ -52,6 +52,9 @@ export default class {
                 }
               }
             })
+
+          //filtre décroissant 
+          bills.sort((a, b) => new Date(b.date) - new Date(a.date))
           console.log('length', bills.length)
           return bills
         })
